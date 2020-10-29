108475fc9ef2ba04c86719b04b2b06fa30793589
hitman1439 <37571052+hitman1439@users.noreply.github.com>
2020-10-29 22:01:08 +0900

    add question model

---
+++ models/question.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const QuestionSchema = mongoose.Schema({
+    
+    id: {
+        type: String,
+        unique : true,
+        required: true,
+    },
+    contents:{
+        type: String,
+        required: true
+    },
+    title:{
+        type : String, 
+        required: true
+    },
+    textBookId : {
+        type : String
+    },
+    major : {
+        type : String
+    },
+    like : {
+        type : Integer
+    },
+    viewCnt : {
+        type : Integer
+    },
+    registeredDate: {
+        type: Date,
+        default: Date.now()
+    },
+});
+
+const Question = (module.exports = mongoose.model('Question', adminSchema));
+
+module.exports = { Question };
