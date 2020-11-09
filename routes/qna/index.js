6765258432e1323b66d43d42c9bcd010a7a805cb
isaac <hitman1439@naver.com>
2020-11-09 17:28:35 +0900

    add question update logic

--- routes/qna/index.js
+++ routes/qna/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { Question } = require('../../models/qna/question');
 
+// 질문 전체조회
 router.get('/question', function(req, res){
     Question.find({}, function(err, qst){
         if(err) return res.status(500).send({success:false, err})
@@ -10,6 +11,7 @@ router.get('/question', function(req, res){
     })
 });
 
+// 질문 상세 조회
 router.get('/question/:id', function (req, res) {
     Question.findOne({id : req.params.id}, function(err, qst){
         if(err) return res.status(500).send({success:false, err})
@@ -17,7 +19,16 @@ router.get('/question/:id', function (req, res) {
         res.status(200).send(qst)
     })
 });
+ 
+// 질문 수정
+router.put('/question/:id', function (req, res) {    
+    Question.updateOne({id : req.params.id}, {$set : req.body}, function (err, qst) {
+        if (err) return res.status(500).send({success:false, err});
+        res.status(200).send("수정이 완료됐습니다.");
+    });
+});
 
+// 질문 등록
 router.post('/question', function(req, res){
     const question = new Question(req.body)
     question.save(function(err, qst){
@@ -26,11 +37,14 @@ router.post('/question', function(req, res){
     })
 }) 
 
+// 질문 삭제
 router.delete('/question/:id', function(req,res){
     Question.findOneAndRemove(req.params.id, function (err, qst) {
         if (err) return res.status(500).send("삭제 실패");
         res.status(200).send("삭제됨.");
     })
 })
+
+
 
 module.exports = router;
